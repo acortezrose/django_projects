e05efd99f19ac4195890e8f5aa1fa408a4a20d21
acortezrose <acortezrose@gmail.com>
2019-02-14 22:51:44 -0500

    borrowd challenge finished

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -81,6 +81,7 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """string for representing the model object"""
