854e0dcebcc60e699f653822abd16ed1fcb78630
acortezrose <acortezrose@gmail.com>
2019-02-21 22:22:45 -0500

    autos crud complete

--- dj4e/home/urls.py
+++ dj4e/home/urls.py
@@ -3,5 +3,5 @@ from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-	path('helloworld', views.helloworld),
+	path('', TemplateView.as_view(template_name='main.html'), name='main'),
 ]
